ac9a1e399828a501f07c475fb28e4a91814ef55a
Mads Risager <madssr2@gmail.com>
2024-03-14 14:25:15 +0100

    Exclude old utils

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open('requirements.txt') as f:
 
 setup(
     name='fastfiz-env',
+    description='Gymnasium environments for FastFiz',
     version='0.0.1',
     license='MIT',
     install_requires=requirements,
     packages=find_packages(where='src', include=[
-                           'fastfiz_env', 'fastfiz_env.*']),
+                           'fastfiz_env'], exclude=['fastfiz_env.utils.*_manager']),
     package_dir={"": "src"},
 )
