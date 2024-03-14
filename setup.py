0c03de0c1ef978ba6ab2b9d7893c9783c52ca569
Mads Risager <madssr2@gmail.com>
2024-03-14 11:01:02 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='fastfizenv',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=requirements,
+    package_dir={"": "src"},
+)
