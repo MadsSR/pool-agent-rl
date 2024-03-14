a3bbf0fbe831bf0581961272745b9aff3efafde6
Mads Risager <madssr2@gmail.com>
2024-03-14 11:43:11 +0100

    Register envs

--- src/fastfiz_env/envs/__init__.py
+++ src/fastfiz_env/envs/__init__.py
@@ -1,2 +1,14 @@
 from .BaseFastFiz import BaseFastFiz
 from .BaseRLFastFiz import BaseRLFastFiz
+
+from gymnasium import register
+
+register(
+    id='BaseFastFiz-v0',
+    entry_point='fastfiz_env.envs:BaseFastFiz',
+)
+
+register(
+    id='BaseRLFastFiz-v0',
+    entry_point='fastfiz_env.envs:BaseRLFastFiz',
+)
